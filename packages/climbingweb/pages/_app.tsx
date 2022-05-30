831feb7ccf39b9e5335ac0245aae3e317be512bc
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2022-05-30 11:25:28 +0900

    chore: redux boilerplate setting for web (#43)

--- packages/climbingweb/pages/_app.tsx
+++ packages/climbingweb/pages/_app.tsx
@@ -2,12 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'next-themes';
+import store from '../src/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider attribute='class'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
