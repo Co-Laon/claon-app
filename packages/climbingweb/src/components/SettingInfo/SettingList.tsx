8cfb219c459e6fed5d28e70ffa9a9fb6e9158b3d
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2022-08-12 20:45:22 +0900

    feat: implement SettingPage - settingList and versionInfo (#88)

---
+++ packages/climbingweb/src/components/SettingInfo/SettingList.tsx
@@ -0,0 +1,21 @@
+
+
+interface ListProps {
+    titleList?: string[];
+    onClick?: ({ }: any) => void;
+}
+
+export const SettingList = ({ titleList, onClick }: ListProps) => {
+
+    return (
+        <>
+            {
+                titleList &&
+                <ul className='mt-10 h-screen w-screen pl-11 flex flex-col flex-end gap-y-6'>
+                    {titleList.map((title, idx) => <li key={`title${idx}`} className='font-normal text-sm' onClick={onClick}>{title}</li>)}
+
+                </ul>
+            }
+        </>
+    );
+};
--- packages/climbingweb/src/components/common/AppBar.tsx
+++ packages/climbingweb/src/components/common/AppBar.tsx
@@ -9,7 +9,7 @@ export function AppBar({ title, leftNode, rightNode }: Props) {
     return (
         <header className='flex flex-row justify-between p-4'>
             {leftNode}
-            <h1>{title}</h1>
+            <h1 className='font-bold'>{title}</h1>
             {rightNode}
         </header>
     );
