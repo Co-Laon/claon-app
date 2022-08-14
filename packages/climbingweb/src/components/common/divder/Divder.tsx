d38e021b86ad911964b7c1389c45e30de142baad
jinwoo_bae <qowlsdn8007@gmail.com>
2022-08-15 01:38:43 +0900

    feat: implement SettingPage >> EditMyInfo view

---
+++ packages/climbingweb/src/components/common/divder/Divder.tsx
@@ -0,0 +1,1 @@
+export const Divder = () => <div className="w-full h-1 bg-gray-300" />;
---
+++ packages/climbingweb/src/components/common/profileImage/ProfileImage.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import CameraIcon from 'climbingweb/src/assets/icon/ic_24_camera_gray800.svg';
+import InstaIcon from 'climbingweb/src/assets/icon/ic_24_instagram.svg';
+import ProfileSkeleton from 'climbingweb/src/assets/icon/ic_72_profile_gray400.svg';
+
+interface ProfileProps {
+  src?: string;
+  icon: 'default' | 'insta';
+}
+
+export const ProfileImage = ({ src, icon }: ProfileProps) => {
+  return (
+    <div className="w-16 h-16 relative">
+      <div className="w-15 h-15 rounded-xl relative">
+        <Image src={src ? src : ProfileSkeleton} alt="profile" />
+      </div>
+      {icon === 'default' ? (
+        <div className="absolute bottom-0 right-0">
+          <Image src={CameraIcon} alt="cameraIcon" />
+        </div>
+      ) : (
+        <div className="absolute">
+          <Image src={InstaIcon} alt="instaIcon" />
+        </div>
+      )}
+    </div>
+  );
+};
