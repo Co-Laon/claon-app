e73ba6bef4c51d74fbceae8d1fcc2d9a0048f72c
jinwoo <qowlsdn8007@gmail.com>
2022-08-25 20:41:36 +0900

    feat: implement CenterPage

---
+++ packages/climbingweb/src/components/CenterInfo/CenterInfoContent.tsx
@@ -0,0 +1,13 @@
+import { ImageList } from '../common/ImageList';
+import { TabBar } from '../common/TabBar';
+
+export const CenterInfoContent = () => {
+
+
+    return (
+        <div>
+            <ImageList />
+            <TabBar />
+        </div>
+    );
+};
--- packages/climbingweb/src/components/common/CenterResult/CenterResult.tsx
+++ packages/climbingweb/src/components/common/CenterResult/CenterResult.tsx
@@ -2,19 +2,16 @@ import Image from 'next/image';
 import React from 'react';
 import StarIcon from 'climbingweb/src/assets/icon/ic_10_rate_black.svg';
 import DefaultCenterImage from 'climbingweb/src/assets/default_image.svg';
+import { CenterProps } from './type';
 
 const CenterResult = ({
   star,
   image,
   name,
-}: {
-  star: number;
-  image?: string;
-  name: string;
-}) => {
+}: CenterProps) => {
   return (
-    <div className="relative w-[150px] h-[120px] rounded-[4px] bg-gray-800 mr-[5px]">
-      <div className="m-1 px-2 py-[2px] z-10 absolute bg-yellow-500 rounded-full text-xs">
+    <div className="relative min-w-[150px] min-h-[120px] rounded-lg bg-gray-800 mr-[5px]">
+      <div className="m-1 px-2 py-[2px] z-10 absolute bg-yellow-500 rounded-full text-xs font-bold">
         <Image src={StarIcon} alt={'starIcon'} />
         {star}
       </div>
@@ -23,7 +20,7 @@ const CenterResult = ({
         alt={'centerImage'}
         layout="fill"
       />
-      <div className="m-1 px-2 py-[2px] right-0 bottom-0 absolute bg-purple-500 rounded-full text-xs">
+      <div className="m-1 px-2 py-[2px] right-0 bottom-0 absolute bg-purple-500 text-white rounded-full text-xs">
         {name}
       </div>
     </div>
