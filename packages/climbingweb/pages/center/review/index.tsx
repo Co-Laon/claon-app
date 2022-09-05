edb29229431d70ba75db4d6d54de2585253d5f3b
jinwoo <qowlsdn8007@gmail.com>
2022-09-05 14:03:55 +0900

    feat: implement center review page

---
+++ packages/climbingweb/pages/center/review/index.tsx
@@ -0,0 +1,29 @@
+import { AppBar } from 'climbingweb/src/components/common/AppBar';
+import { NormalButton } from 'climbingweb/src/components/common/button/Button';
+import {
+  BackButton,
+  Empty,
+} from 'climbingweb/src/components/common/IconButton';
+import { StarRating } from 'climbingweb/src/components/common/StarRating';
+import TextArea from 'climbingweb/src/components/common/TextArea/TextArea';
+import 'react-spring-bottom-sheet/dist/style.css';
+export default function ReportPage({}) {
+  return (
+    <section className="mb-footer">
+      <AppBar leftNode={<BackButton />} title="" rightNode={<Empty />} />
+      <div className="px-5 flex flex-col gap-4">
+        <div className="flex flex-col gap-2.5">
+          <h2 className="text-xl font-extrabold leading-6">
+            리뷰를 작성해주세요
+          </h2>
+          <StarRating count={5} size="md" readOnly={false} initialValue={5} />
+          <TextArea
+            setData={() => {}}
+            placeholder="요청 내용을 자세히 입력해주세요."
+          />
+        </div>
+        <NormalButton>완료</NormalButton>
+      </div>
+    </section>
+  );
+}
