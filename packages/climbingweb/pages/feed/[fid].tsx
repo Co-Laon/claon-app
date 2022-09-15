f90515e2cd32797ad270cb6ded1de5c63ca555a6
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2022-09-15 11:02:51 +0900

    feat: 단일 피드 화면 구현 (#111)

---
+++ packages/climbingweb/pages/feed/[fid].tsx
@@ -0,0 +1,15 @@
+import { AppBar } from 'climbingweb/src/components/common/AppBar';
+import {
+  AppLogo,
+  ModifiedButton,
+} from 'climbingweb/src/components/common/IconButton';
+import HomeFeed from 'climbingweb/src/components/HomeFeed/HomeFeed';
+
+export default function FeedPage({}) {
+  return (
+    <section>
+      <AppBar leftNode={<AppLogo />} rightNode={<ModifiedButton />} />
+      <HomeFeed imageList={[]} holdList={[]} />
+    </section>
+  );
+}
