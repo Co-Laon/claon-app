457ef34aa93e1c11a145c6b35bdcdec5223fb3dd
ywj3493 <ywj3493@nate.com>
2022-09-21 15:06:57 +0900

    feat: next.js 이미지 도메인 추가

--- packages/climbingweb/next.config.js
+++ packages/climbingweb/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com','*', 'bunny.jjalbot.com'],
+    domains: [
+      'images.unsplash.com',
+      '*',
+      'bunny.jjalbot.com',
+      'claon-server.s3.ap-northeast-2.amazonaws.com',
+    ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
@@ -13,9 +18,9 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
-    })
+    });
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
