8263af529e4fdfaf38cb34a0d3cedce21232d85a
ywj3493 <ywj3493@nate.com>
2022-10-06 14:34:04 +0900

    feat: NextButton 완료 버튼 콜백 함수 추가

--- packages/climbingweb/src/components/common/AppBar/NextButton.tsx
+++ packages/climbingweb/src/components/common/AppBar/NextButton.tsx
@@ -1,15 +1,25 @@
 import { Dispatch, SetStateAction, useCallback } from 'react';
 
+export const NextButton = ({
+  pageState,
+  setPageState,
+  onSubmit,
+}: {
+  pageState: string;
+  setPageState: Dispatch<SetStateAction<string>>;
+  onSubmit: any;
+}) => {
+  const handleNextButton = useCallback(() => {
+    if (pageState === 'first') {
+      setPageState('second');
+    } else {
+      onSubmit();
+    }
+  }, [pageState, setPageState, onSubmit]);
 
-export const NextButton = ({ pageState, setPageState }: { pageState: string, setPageState: Dispatch<SetStateAction<string>> }) => {
-
-    const handleNextButton = useCallback(() => {
-        if (pageState === 'first') {
-            setPageState('second');
-        } else {
-            console.log('완료');
-        }
-    }, [pageState, setPageState]);
-
-    return <span className='text-sm text-purple-500' onClick={handleNextButton}>{pageState === 'first' ? '다음' : '완료'}</span>;
+  return (
+    <span className="text-sm text-purple-500" onClick={handleNextButton}>
+      {pageState === 'first' ? '다음' : '완료'}
+    </span>
+  );
 };
