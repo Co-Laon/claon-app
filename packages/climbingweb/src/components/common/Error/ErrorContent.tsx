51edc9f49d5b2e0dc59c46606400ca1ac12e7ebb
ywj3493 <ywj3493@nate.com>
2022-11-18 10:45:50 +0900

    feat: 공통 에러 컴포넌트 서버 비즈니스 에러 추가

--- packages/climbingweb/src/components/common/Error/ErrorContent.tsx
+++ packages/climbingweb/src/components/common/Error/ErrorContent.tsx
@@ -1,11 +1,19 @@
-import { ServerError } from 'climbingweb/types/common';
+import { ServerBusinessError, ServerError } from 'climbingweb/types/common';
 import React from 'react';
 
 interface ErrorContentProps {
-  error: ServerError;
+  error: ServerError | ServerBusinessError;
 }
 
 // 공통 error 컴포넌트, 추후 변경 예정
-const ErrorContent = ({ error }: ErrorContentProps) => <div>{error}</div>;
+const ErrorContent = ({ error }: ErrorContentProps) => {
+  if (error.hasOwnProperty('errorCode')) {
+    const { message } = error as ServerBusinessError;
+    return <div>{message}</div>;
+  } else {
+    const { error: message } = error as ServerError;
+    return <div>{message}</div>;
+  }
+};
 
 export default ErrorContent;
