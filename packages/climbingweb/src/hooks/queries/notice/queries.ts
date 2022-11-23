8eb8fcada7fe906a198155370bd3e5f0c3dd0dd3
ywj3493 <ywj3493@nate.com>
2022-11-23 10:08:47 +0900

    refactor: notice 컨트롤러 query key factory 적용

---
+++ packages/climbingweb/src/hooks/queries/notice/queries.ts
@@ -0,0 +1,17 @@
+import { NoticeReponse } from './../../../../types/response/notice/index.d';
+import { Pagination } from 'climbingweb/types/common';
+import axios from 'axios';
+
+/**
+ * GET /notices api의 query 함수
+ *
+ * @returns axiosResponse.data
+ */
+export const getNoticeList = async (pageParam = 0) => {
+  const { data } = await axios.get<Pagination<NoticeReponse>>('/notices', {
+    params: {
+      page: pageParam,
+    },
+  });
+  return data;
+};
