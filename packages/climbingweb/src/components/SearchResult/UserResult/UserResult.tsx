45da172e5f9f7c9d937ddeb4eee71be3e8731759
ywj3493 <ywj3493@nate.com>
2022-12-26 20:33:04 +0900

    fix: 171 검색 버그 수정

--- packages/climbingweb/src/components/SearchResult/UserResult/UserResult.tsx
+++ packages/climbingweb/src/components/SearchResult/UserResult/UserResult.tsx
@@ -1,7 +1,7 @@
 import { useCreateLaon } from 'climbingweb/src/hooks/queries/laon/queryKey';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { ProfileImage } from '../../common/profileImage/ProfileImage';
 
 interface UserProps {
   imagePath: string;
@@ -30,13 +30,7 @@ const UserResult = ({ imagePath, isLaon, nickname }: UserProps) => {
   return (
     <div className="relative flex flex-col mx-2 items-center text-center w-[75px] h-[110px] rounded-[4px] border-gray-400 border-2 text-[8px] p-1">
       <div onClick={handleUserResultClick}>
-        <Image
-          className="rounded-full"
-          src={imagePath}
-          alt={'raonImage'}
-          width={'45px'}
-          height={'45px'}
-        />
+        <ProfileImage src={imagePath} size={45} />
         {nickname}
       </div>
       <button
