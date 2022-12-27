48195b1079550a0d12dff28fea2cd485b812fc24
ywj3493 <ywj3493@nate.com>
2022-12-27 17:17:27 +0900

    fix: #176 별점 컴포넌트 css 오류 수정

--- packages/climbingweb/src/components/common/StarRating/index.tsx
+++ packages/climbingweb/src/components/common/StarRating/index.tsx
@@ -15,8 +15,11 @@ export const StarRating = ({
 }: RatingProps) => {
   const stars = Array(count * 2)
     .fill(1)
-    .map((star, idx) => (idx % 2 === 0 ? star : star + 1))
-    .map(String);
+    .map((star, idx) => (idx % 2 === 0 ? star : star + 1));
+
+  // tailwind jit 모드 버그로 인한 것으로 판단되어 임시 해결책 사용
+  const leftHalf = 'bg-yellow-500 mask mask-star-2 mask-half-1';
+  const rightHalf = 'bg-yellow-500 mask mask-star-2 mask-half-2';
 
   const [value, setValue] = useState(initialValue);
 
@@ -24,22 +27,21 @@ export const StarRating = ({
     if (!readOnly) setValue(num / 2);
     console.log(value);
   };
+
   return (
-    <>
-      <form className={`rating rating-${size} rating-half relative`}>
-        {stars.map((star, idx) => (
-          <input
-            key={`rating${idx}`}
-            type="radio"
-            name="rating"
-            multiple
-            readOnly={readOnly}
-            checked={value * 2 >= idx + 1}
-            onChange={() => handleCheck(idx + 1)}
-            className={`bg-yellow-500 mask mask-star-2 mask-half-${star}`}
-          />
-        ))}
-      </form>
-    </>
+    <form className={`rating rating-${size} rating-half relative`}>
+      {stars.map((star, idx) => (
+        <input
+          key={`rating${idx}`}
+          type="radio"
+          name="rating"
+          multiple
+          readOnly={readOnly}
+          checked={value * 2 >= idx + 1}
+          onChange={() => handleCheck(idx + 1)}
+          className={star % 2 === 0 ? rightHalf : leftHalf}
+        />
+      ))}
+    </form>
   );
 };
