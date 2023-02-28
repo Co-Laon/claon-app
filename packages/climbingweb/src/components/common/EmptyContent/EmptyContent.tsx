9ab3493065af85f5dab121182e0d31eacd553925
LUCAssYA <42572954+LUCAssYA@users.noreply.github.com>
2023-02-28 10:24:09 +0900

    style: [CSS]#암장 (#252)
    * style: #암장
    
    * style: 4px 단위로 수정

--- packages/climbingweb/src/components/common/EmptyContent/EmptyContent.tsx
+++ packages/climbingweb/src/components/common/EmptyContent/EmptyContent.tsx
@@ -6,7 +6,9 @@ interface EmptyContentProps {
 
 // 데이터가 없을 때 사용하는 공통 empty 컴포넌트, message를 받아서 empty 메시지를 띄워준다.
 const EmptyContent = ({ message }: EmptyContentProps) => (
-  <div className="w-full text-center leading-[10]">{message}</div>
+  <div className="flex w-full justify-center items-center h-[16.7vh]">
+    {message}
+  </div>
 );
 
 export default EmptyContent;
