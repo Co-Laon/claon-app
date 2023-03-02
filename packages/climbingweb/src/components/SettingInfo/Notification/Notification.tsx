a2886ac443f2180c31a0517b67533af1b647879e
LUCAssYA <woals821@Naver.com>
2023-03-02 15:11:15 +0900

    style: 공지 상세정보 수정

--- packages/climbingweb/src/components/SettingInfo/Notification/Notification.tsx
+++ packages/climbingweb/src/components/SettingInfo/Notification/Notification.tsx
@@ -2,7 +2,7 @@ import { NoticeReponse } from 'climbingweb/types/response/notice';
 
 export const Notification = ({ title, createdAt, content }: NoticeReponse) => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 pl-3 pr-1">
       <div className="flex flex-col gap-1">
         <h2 className="text-base font-bold">{title}</h2>
         <p className="text-xs font-medium leading-[18px] text-[#808080]">
--- packages/climbingweb/src/hooks/queries/notice/queries.ts
+++ packages/climbingweb/src/hooks/queries/notice/queries.ts
@@ -13,6 +13,5 @@ export const getNoticeList = async (pageParam = 0) => {
       page: pageParam,
     },
   });
-
   return data;
 };
