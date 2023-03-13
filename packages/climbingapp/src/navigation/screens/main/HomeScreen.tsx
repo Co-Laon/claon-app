2f3f61dc751f5edbe325e39110667223a3603199
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2023-03-13 10:59:47 +0900

    fix: ios 빌드 수정, 카카오로그인 라이브러리 변경 (#304)
    * fix: ios 빌드 에러 수정, kakao login 라이브러리 변경
    
    * fix: ios 노치화면 area 수정
    
    * style: ios일 때 로그인 버튼 flex 더 크게 적용

--- packages/climbingapp/src/navigation/screens/main/HomeScreen.tsx
+++ packages/climbingapp/src/navigation/screens/main/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import CustomWebView from 'climbingapp/src/component/webview/CustomWebView';
 import React from 'react';
 import Config from 'react-native-config';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function HomeScreen() {
-  return <CustomWebView url={Config.URL} />;
+  return <SafeAreaView edges={['right', 'left', 'top']} style={{ flex: 1, backgroundColor: 'white' }}><CustomWebView url={Config.URL} /></SafeAreaView>;
 }
 
 export default HomeScreen;
